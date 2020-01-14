6cf20bb22b7c0fb2376f8f29c4f8069c5a346e2e
Sunil Kurahatti <skurahatti@seic.com>
2020-01-14 16:36:54 +0530

    milestone 2 project list complete

---
+++ Milestone_2_lists/app.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 14:14:28 2020
+
+@author: skurahatti
+
+"""
+from utilsss import database
+
+USER_CHOICE="""
+Enter:
+-'a' to add new book
+-'l' to list all books
+-'r' to mark book as read
+-'d' to delete a book
+-'q' to quit
+
+your chouces here:
+"""
+
+def menu():
+    user_input=input(USER_CHOICE)
+    while user_input!='q':
+        if user_input=='a':
+            prompt_add_book()
+        elif user_input=='l':
+            list_books()
+        elif user_input=='r':
+            prompt_read_book()
+        elif user_input=='d':
+            prompt_delete_book()
+        else:
+            print("\nPlease enter a valid input")
+        user_input=input(USER_CHOICE) 
+        
+    
+
+def prompt_add_book():
+    name=input("Enter name of the book: ")
+    author=input("Enter the author name: ")
+    database.add_book(name,author)
+    
+
+def list_books():
+    books=database.get_all_books()
+    for book in books:
+        read='YES' if book['read']==True else 'NO'
+        print(f"{book['name']} written by {book['author']} read status is  {read}")
+              
+
+def prompt_read_book():
+    name=input("please enter name of the book to mak as read: ")
+    database.mark_as_read(name)
+    
+    
+def prompt_delete_book():
+    name=input("Enter name of the book to delete: ")
+    database.delete_book(name)
+
+
+
+    
+menu()
---
+++ Milestone_2_lists/utilsss/database.py
@@ -0,0 +1,20 @@
+
+
+books=[]
+
+def add_book(name,author):
+    books.append({'name':name,'author':author,'read':False})
+    
+    
+def get_all_books():
+    return books
+
+
+def mark_as_read(name):
+    for book in books:
+        if book['name']==name:
+            book['read']=True
+            
+def delete_book(name):
+    global books
+    books=[book for book in books if book['name']!=name ]
