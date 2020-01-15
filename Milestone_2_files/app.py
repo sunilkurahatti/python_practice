5cd2a4f95fa5d7bb28d50cf9e55c3ecd6b040aae
Sunil <sunilkurahatti@gmail.com.com>
2020-01-15 17:36:38 +0530

    milestone2file

--- Milestone_2_files/app.py
+++ Milestone_2_files/app.py
@@ -7,7 +7,7 @@ Created on Tue Jan 14 14:14:28 2020
 """
 from utilsss import database
 
-USER_CHOICE="""
+USER_CHOICE = """
 Enter:
 -'a' to add new book
 -'l' to list all books
@@ -18,45 +18,57 @@ Enter:
 your chouces here:
 """
 
+
 def menu():
-    user_input=input(USER_CHOICE)
-    while user_input!='q':
-        if user_input=='a':
+    user_input = input(USER_CHOICE)
+    while user_input != 'q':
+        if user_input == 'a':
             prompt_add_book()
-        elif user_input=='l':
+        elif user_input == 'l':
             list_books()
-        elif user_input=='r':
+        elif user_input == 'r':
             prompt_read_book()
-        elif user_input=='d':
+        elif user_input == 'd':
             prompt_delete_book()
         else:
             print("\nPlease enter a valid input")
-        user_input=input(USER_CHOICE)
-
+        user_input = input(USER_CHOICE)
 
 
 def prompt_add_book():
-    name=input("Enter name of the book: ")
-    author=input("Enter the author name: ")
-    database.add_book(name,author)
+    name = input("Enter name of the book: ")
+    author = input("Enter the author name: ")
+    database.add_book(name, author)
 
 
 def list_books():
-    books=database.get_all_books()
+    books = database.get_all_books()
+
     for book in books:
-        read='YES' if book['read']==True else 'NO'
-        print("{} written by {} read status is {}".format(book['name'],book['author'],read))
+        print(book)
+        # read='YES' if book[2]=='1' else 'NO'
+        print("{} written by {} read status is {}".format(book[0], book[1], book[2]))
+
 
 def prompt_read_book():
-    name=input("please enter name of the book to mak as read: ")
+    name = input("please enter name of the book to mak as read: ")
     database.mark_as_read(name)
 
 
 def prompt_delete_book():
-    name=input("Enter name of the book to delete: ")
+    name = input("Enter name of the book to delete: ")
     database.delete_book(name)
 
 
+#
+# books_file='books_file.txt'
+# with open(books_file,'r') as file:
+#    lines=[line.strip().split(',') for line in file.readlines()]
+#    print(lines)
+# books=[
+#    {'name':line[0],'author':line[1],'read':line[2]}
+#    for line in lines]
+# print(books)
 
 
 menu()
--- Milestone_2_files/utilsss/database.py
+++ Milestone_2_files/utilsss/database.py
@@ -3,22 +3,28 @@ store data in text file in csv format
 format will be : name,author,read
 """
 
-books_file='books_file.txt'
+books_file = 'books_file.txt'
 
-def add_book(name,author):
-    with open(books_file,'a') as file:
-        file.write("[{},{},{}]\n".format(name,author,0))
+
+def add_book(name, author):
+    with open(books_file, 'a') as file:
+        file.write("{},{},{}\n".format(name, author, 0))
 
 
 def get_all_books():
-    return books
+    with open(books_file, 'r') as file:
+        lines = [line.strip().split(',') for line in file.readlines()]
+
+    return [
+        {'name': line[0], 'author': line[1], 'read': line[2]}
+        for line in lines]
 
 
 def mark_as_read(name):
     for book in books:
-        if book['name']==name:
-            book['read']=True
+        if book['name'] == name:
+            book['read'] = True
+
 
 def delete_book(name):
-    global books
-    books=[book for book in books if book['name']!=name ]
+    books = [book for book in books if book['name'] != name]
